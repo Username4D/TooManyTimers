43e14299a480d9313bf5a16073be29da51afdd27
Username4D <studio@kremzow.org>
2025-09-22 20:29:23 +0200

    fixed timer issue

--- Scripts/timer.gd
+++ Scripts/timer.gd
@@ -7,5 +7,7 @@ func _ready() -> void:
 	$Timer.start()
 
 func _process(delta: float) -> void:
-	$time_left.text = var_to_str(floori($Timer.time_left / 60)) + ":" + var_to_str(int($Timer.time_left) % 60)
+	var text = var_to_str(floori($Timer.time_left / 60)) + ":"
+	text += var_to_str(int($Timer.time_left) % 60) if len(var_to_str(int($Timer.time_left) % 60)) == 2 else "0" + var_to_str(int($Timer.time_left) % 60)
 	$ColorRect.size.y = 112 * ($Timer.time_left / $Timer.wait_time)
+	$time_left.text = text
